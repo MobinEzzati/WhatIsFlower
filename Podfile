85274b6748627f2e8c6ecac36617403dcd080c9f
mobinezzati662 <mobinezzati662@gmail.com>
2022-01-05 23:24:58 +0330

    first commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+
+target 'WhatFlower' do
+   pod 'Alamofire', '~> 5.5'
+       pod 'SwiftyJSON', '~> 4.0'
+
+
+end
--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,12 +6,70 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import SwiftyJSON
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php?format=json&action= query"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        navigationController?.navigationBar.backgroundColor = .magenta
+            
+    }
+    @IBAction func takePhoto(_ sender: UIBarButtonItem) {
+        present(imagePicker
+                , animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            
+            guard let ciImage = CIImage(image: selectedImage) else {
+                
+                fatalError("this is not false ")
+            }
+            
+            detect(image: ciImage)
+            
+        }
+        
+        imagePicker.dismiss(animated: false, completion: nil)
+    }
+    
+    func detect(image : CIImage){
+        guard let imageModel =  try? VNCoreMLModel(for: FlowerShop().model
+        )else {
+            fatalError(" problem for loading image")
+        }
+        let request = VNCoreMLRequest(model: imageModel) { request, error  in
+            guard  let results = request.results as? [VNClassificationObservation] else {
+                fatalError("we have problem to classifaction model")
+            }
+            if let firstRes = results.first{
+                self.navigationController?.navigationBar.topItem?.title = "\(firstRes.identifier)"
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try! handler.perform([request])
+            
+        }catch {
+            print(error)
+        }
+    }
+    
+    func requestInfo(){
+        
     }
 
 
